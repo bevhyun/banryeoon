99bd4d1249f04452e242a7b657f4ed290c9e8ca2
이다현 <83507495+bevhyun@users.noreply.github.com>
2025-06-02 04:06:35 +0900

    Delete banryeoon_back_2fin directory

--- banryeoon_back_2fin/SqlFIle/Certification.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Certification (
-  cert_id    INT           NOT NULL AUTO_INCREMENT,
-  user_id    VARCHAR(50)   NULL     UNIQUE,
-  content    TEXT          NULL,
-  cerStep    VARCHAR(50)   NULL,
-  file       VARCHAR(255)  NULL,
-  PRIMARY KEY (cert_id)
-);
